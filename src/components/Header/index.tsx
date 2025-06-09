5cdc3c8532331ef28d28c7f8b8bbec51a0703b9e
naelem <ellenxxy19@gmail.com>
2025-06-09 16:38:34 -0300

    [FEAT] - Adding modal that opens when the button is clicked

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from '../../assets/logo.svg'
+import { TransactionsModal } from "../TransactionsModal";
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="logo" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <TransactionsModal />
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
