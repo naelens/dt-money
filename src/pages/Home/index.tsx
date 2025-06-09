0bd6c31beaee6163641f279c8cd789f609da7799
naelem <ellenxxy19@gmail.com>
2025-06-09 14:35:52 -0300

    [FEAT] - Table structure is finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Home() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="40%">Desenvolvimento de Site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2025</td>
+                        </tr>
+                        <tr>
+                            <td width="40%">Hamburguer</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
