a62a0efff58517f22fe95b9f788393c9a6d1c6d2
naelem <ellenxxy19@gmail.com>
2025-06-10 17:00:28 -0300

    [FEAT] - Iterating over a list of transactions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionContext } from "../../contexts/TransactionsContext";
 
 export function Home() {
+    const { transactions } = useContext(TransactionContext)
+
     return (
         <div>
             <Header />
@@ -11,28 +15,23 @@ export function Home() {
 
             <TransactionsContainer>
                 <SearchForm />
+
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="40%">Desenvolvimento de Site</td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2025</td>
-                        </tr>
-                        <tr>
-                            <td width="40%">Hamburguer</td>
-                            <td>
-                                <PriceHighLight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2025</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="40%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighLight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighLight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
