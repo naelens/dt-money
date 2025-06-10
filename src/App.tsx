46201fd99cf56fcfacd26a16751163599f16b583
naelem <ellenxxy19@gmail.com>
2025-06-10 17:01:19 -0300

    [FEAT] - Transaction provider added to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Home } from "./pages/Home";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Home />
+        <TransactionsProvider>
+          <Home />
+        </TransactionsProvider>
     </ThemeProvider>
   )
 }
